550b3dd11c4e8fdcf697babd5e1097c154490a23
tarun-upadhyay <tarunu88@gmail.com>
2023-08-11 23:46:06 +0530

    added tootltip

--- src/app/Components/Skills/ListSkill.tsx
+++ src/app/Components/Skills/ListSkill.tsx
@@ -1,23 +1,31 @@
+"use client"
 import Image from "next/image";
+import { Tooltip as ReactTooltip, Tooltip } from "react-tooltip";
 import React, { FC } from "react";
 interface SkillProps {
   image: any;
   alt: string;
   rounded: boolean;
   id: number;
- 
 }
 const ListSkill: FC<SkillProps> = ({ image, alt, rounded }) => {
   return (
-    <div className="mx-auto duration-200  hover:scale-110 m-1 p-3">
+    <div
+      className="mx-auto duration-200  hover:scale-110 m-1 p-3"
+      
+
+    >
+      <Tooltip id="my-tooltip" />
       <Image
         className={`${
           rounded ? "rounded-[50%]" : "rounded"
-        }  w-full object-fit h-40 `}
+        }  w-full object-fit  md:h-36 sm:28`}
         src={image}
         alt={alt}
         width={200}
         height={200}
+        data-tooltip-id="my-tooltip"
+      data-tooltip-content={alt} 
       />
     </div>
   );
